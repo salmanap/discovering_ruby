63ac668aa3fccf38ac058c4131755f12f23738be
Salman Paracha <salman.paracha@gmail.com>
2012-06-04 21:43:26 -0400

    adding empty files for discovering ruby

---
+++ 101.rb
@@ -0,0 +1,13 @@
+#This file basically helps understand the basic principles of ruby
+# and is intended to refresh chapters 1 & 2 of the Ruby (2008) book
+
+class Hello
+  
+  include Enumerable
+  
+  def some_method()
+    puts "some method"
+  end
+  
+end
+
